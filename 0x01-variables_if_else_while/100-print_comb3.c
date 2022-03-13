a024cb4de1d3ce474b2cbf6f5f7e5f759b3f2fd3
Demex1 <99673005+Demex1@users.noreply.github.com>
2022-03-13 06:23:00 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@ for (tens = 0; tens <= 9; tens++)
 {
 for (ones = tens + 1; ones <= 9; ones++)
 {
-	  putchar(tens + '0');
-	  putchar(ones + '0');
+putchar(tens + '0');
+putchar(ones + '0');
 
 if (tens < 8)
 {
