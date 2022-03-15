0c5878c5637ed0034628a500502b015f7e4dca15
demex1 <komoladefemilade@gmail.com>
2022-03-15 13:33:13 -0700

    jk

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef _putchar
-#define _putchar
+#ifndef main
+#define main
 
-int putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
