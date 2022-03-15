8029f274b55c91e7d7e4bbcf8aa1afcf6d1c244a
demex1 <komoladefemilade@gmail.com>
2022-03-15 15:51:30 -0700

    update!

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,11 +8,11 @@
 */
 int main(void)
 {
-int n1 = 0, n2 = 1, n3, i;       
+int n1 = 0, n2 = 1, n3, i;
 for (i = 2; i <= 50; ++i)
 {
 n3 = n1 + n2;
-printf("%d, ", n3);
+printf("%ld, ", n3);
 n1 = n2;
 n2 = n3;
 }
