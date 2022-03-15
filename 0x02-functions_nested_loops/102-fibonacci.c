3b015af15b7549e4b9af67ca57b40835bca0f331
demex1 <komoladefemilade@gmail.com>
2022-03-15 15:38:14 -0700

    hi

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 int n1 = 0,n2 = 1,n3,i;      
-printf("\n%d %d",n1,n2);//printing 0 and 1    
+printf("%d %d", ",n1,n2);//printing 0 and 1    
 for(i=2;i<=50;++i)//loop starts from 2 because 0 and 1 are already printed    
 {    
 n3=n1+n2;    
