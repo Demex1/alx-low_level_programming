09b4ba2a5e600c8ed9e23dec4a2ddbb436a1e87c
demex1 <komoladefemilade@gmail.com>
2022-03-17 00:26:59 -0700

    update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,13 @@
 #ifndef main_H_
 #define main_H_
 
+#include <stdio.h>
+
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
