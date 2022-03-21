e4ba041f5f5a862c0365dbf8e9f5b264adf25977
demex1 <komoladefemilade@gmail.com>
2022-03-21 03:45:29 -0700

    uo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * pointer to an int as parameter
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+int j;
+*p = &j;
+*p = 98;
+
+return(0);
+
+}
