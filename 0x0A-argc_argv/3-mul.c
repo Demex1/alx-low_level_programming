87c5324f8f5c4f95cae15f91fa39100ef1d211e1
demex1 <komoladefemilade@gmail.com>
2022-04-01 13:12:56 -0700

    update 5

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - mul 2 argv
- * @argc:int
- * @argv:string
- * Return:int
- */
+ * main - multiply two numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: result of multiplication or 1
+ **/
 int main(int argc, char *argv[])
 {
-int x, i, j;
+int a, b, result;
 
-if (argc != 2)
+if (argc <= 2)
 {
 printf("Error\n");
 return (1);
 }
-else
-{
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-x = i * j;
-printf("%d\n", x);
-}
+/* atoi convert a string to an int */
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
+printf("%d\n", result);
 return (0);
 }
