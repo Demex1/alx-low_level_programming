e5b7849a7094a86fbbfa9554cc06251521942b3a
demex1 <komoladefemilade@gmail.com>
2022-04-07 12:07:42 -0700

    second update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,16 +1,16 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- **_realloc -  a function that reallocates a memory block using malloc and free
- *@ptr : pointer
- *@old_size : int
- *@new_size : int
- *Return: pointer
- */
+ * *_realloc -  reallocates a memory block using malloc and free
+ * @ptr: void pointer
+ * @old_size: already allocated size
+ * @new_size: new size to allocate
+ * Return: pointer to newly allocated memory or null
+ **/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *tmp;
+unsigned char *np;
+unsigned int i;
 
 if (new_size == old_size)
 return (ptr);
@@ -21,9 +21,31 @@ return (NULL);
 }
 if (ptr == NULL)
 {
-ptr = malloc(new_size);
+ptr = malloc(new_size * sizeof(void *));
+if (ptr == NULL)
+return (NULL);
 return (ptr);
 }
-tmp = malloc(new_size);
+np = malloc(new_size * sizeof(char));
+if (np == NULL)
+return (NULL);
+i = 0;
+if (new_size > old_size)
+{
+while (i < old_size)
+{
+np[i] = ((char *)ptr)[i];
+i++;
+}
 free(ptr);
-return (tmp);
+return (np);
+}
+/* if new_size < old_size */
+while (i < new_size)
+{
+np[i] = ((char *)ptr)[i];
+i++;
+}
+free(ptr);
+return (np);
+}
